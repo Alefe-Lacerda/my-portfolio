47c5a4642582a779f3efafefb9f79a8aa4aaaece
Álefe Lacerda <alefeguimel12@gmail.com>
2024-05-17 12:01:54 -0300

    feat: inits navbar

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,28 @@
+import { AppBar, MenuItem, Toolbar, styled } from "@mui/material"
+import theme from "../../theme"
+import { teal } from "@mui/material/colors"
+
+
+
+const NavBar = () => {
+
+    const StyledToobar = styled(Toolbar)(()=> ({
+        display: "flex",
+        justifyContent: "space-evenly",
+        background: theme.palette.secondary.main
+    }))
+
+    return (
+      <>
+        <AppBar position="absolute">
+            <StyledToobar>
+                <MenuItem>About</MenuItem>
+                <MenuItem>Skills</MenuItem>
+                <MenuItem>Project</MenuItem>
+            </StyledToobar>
+        </AppBar>
+      </>
+    )
+  }
+  
+  export default NavBar
